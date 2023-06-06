3b8078c9aec31240685bf0032306c55c57dd08d0
penbipaipu <s1f102300122@iniad.org>
2023-06-06 21:59:25 +0900

    迷走

--- server/repository/MakeBoard.tsx
+++ server/repository/MakeBoard.tsx
@@ -1,23 +1,21 @@
-import { AroundBoard } from './AroundBoard';
-
 export const makeBoard = {
   makeBoard: () => {
-    for (let y = 0; y < 8; y += 1) {
-      for (let x = 0; x < 8; x += 1) {
-        const around = [
-          [-1, 0],
-          [-1, 1],
-          [0, 1],
-          [1, 1],
-          [1, 0],
-          [1, -1],
-          [0, -1],
-          [-1, -1],
-        ];
-        for (const i of around) {
-          AroundBoard.aroundBoard(y, x, i);
-        }
-      }
-    }
+    // for (let y = 0; y < 8; y += 1) {
+    //   for (let x = 0; x < 8; x += 1) {
+    //     const around = [
+    //       [-1, 0],
+    //       [-1, 1],
+    //       [0, 1],
+    //       [1, 1],
+    //       [1, 0],
+    //       [1, -1],
+    //       [0, -1],
+    //       [-1, -1],
+    //     ];
+    //     for (const i of around) {
+    //       AroundBoard.aroundBoard(y, x, i);
+    //     }
+    //   }
+    // }
   },
 };
