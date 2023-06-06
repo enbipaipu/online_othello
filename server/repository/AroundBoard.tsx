ec7db6dab38d71f9ab86ebbed397479e54ce928c
penbipaipu <s1f102300122@iniad.org>
2023-06-06 21:59:25 +0900

    迷走

--- server/repository/AroundBoard.tsx
+++ server/repository/AroundBoard.tsx
@@ -1,12 +1,10 @@
-import { boardRepository } from './boardRepository';
-
 export const AroundBoard = {
   aroundBoard: (y: number, x: number, i: number[]) => {
-    for (let dis = 1; dis < 8; dis += 1) {
-      const board = boardRepository.getBoard();
-      if (board[y + i[0] * dis][x + i[1] * dis] === 1) {
-        //
-      }
-    }
+    // for (let dis = 1; dis < 8; dis += 1) {
+    //   const board = boardRepository.getBoard();
+    //   if (board[y + i[0] * dis][x + i[1] * dis] === 1) {
+    //     //
+    //   }
+    // }
   },
 };
