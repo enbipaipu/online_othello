1692e2568e8e0294367c1a1b5ccaf46bdb6e9a7c
penbipaipu <s1f102300122@iniad.org>
2023-06-12 12:21:04 +0900

    攻略１

--- server/repository/boardRepository.ts
+++ server/repository/boardRepository.ts
@@ -15,7 +15,7 @@ const board: BoardArr = [
   [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-const around = [
+const aroundS = [
   [-1, 0],
   [-1, 1],
   [0, 1],
@@ -25,51 +25,55 @@ const around = [
   [0, -1],
   [-1, -1],
 ];
+
+let passThrough = false;
 const boardTerms = function (
   y: number,
   x: number,
-  s: number[],
+  around: number[],
   turnColor: number,
   distance: number
 ) {
-  let passThrough = false;
-  if (board[y + s[0] * distance][x + s[1] * distance] === 3 - turnColor) {
+  if (board[y + around[0] * distance][x + around[1] * distance] === 3 - turnColor) {
     passThrough = true;
-  } else if (board[y + s[0] * distance][x + s[1] * distance] === turnColor) {
+  } else if (board[y + around[0] * distance][x + around[1] * distance] === turnColor) {
     if (passThrough) {
       for (let i = distance; i > -1; i--) {
-        board[y + s[0] * i][x + s[1] * i] = turnColor;
+        board[y + around[0] * i][x + around[1] * i] = turnColor;
       }
     }
   }
-  return board;
 };
 
-const distanceBoard = function (y: number, x: number, s: number[], turnColor: number) {
+const distanceBoard = function (y: number, x: number, around: number[], turnColor: number) {
   let ok = true;
-
-  if (ok) {
-    for (let distance = 1; distance < 3; distance += 1) {
+  passThrough = false;
+  for (let distance = 1; distance < 8; distance += 1) {
+    if (ok) {
       if (
-        board[y + s[0] * distance] === undefined ||
-        board[y + s[0] * distance][x + s[1] * distance] === 0
+        board[y + around[0] * distance] === undefined ||
+        board[y + around[0] * distance][x + around[1] * distance] === 0
       ) {
         ok = false;
       } else {
-        boardTerms(y, x, s, turnColor, distance);
+        boardTerms(y, x, around, turnColor, distance);
       }
     }
   }
-  return board;
 };
+
+let turn = 1;
 
 export const boardRepository = {
   getBoard: (): BoardArr => board,
   clickBoard: (params: Pos, userId: UserId): BoardArr => {
-    if (board[params.y][params.x] === 0) {
-      for (const s of around) {
-        distanceBoard(params.y, params.x, s, userColorRepository.getUserColor(userId));
+    if (turn === userColorRepository.getUserColor(userId)) {
+      if (board[params.y][params.x] === 0) {
+        for (const around of aroundS) {
+          distanceBoard(params.y, params.x, around, userColorRepository.getUserColor(userId));
+        }
       }
+      turn = 3 - turn;
     }
     return board;
   },
