171e8abe68658db9592314e801898c670794a293
penbipaipu <s1f102300122@iniad.org>
2023-06-20 22:11:57 +0900

    dannzyonn9

--- server/api/rooms/board/controller.ts
+++ server/api/rooms/board/controller.ts
@@ -1,6 +1,7 @@
 import { defineController } from './$relay';
-
 export default defineController(() => ({
   get: () => ({ status: 200, body: 'Hello' }),
-  post: () => ({ status: 200, body: 'Created' }),
+  post: ({  }) => {
+    return { status: 200, body:  };
+  },
 }));
