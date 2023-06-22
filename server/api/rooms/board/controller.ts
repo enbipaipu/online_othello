4db7f5d1c9fdf77eada5a82c4327a21f910c64e2
penbipaipu <s1f102300122@iniad.org>
2023-06-22 21:57:59 +0900

    dannzyonn12

--- server/api/rooms/board/controller.ts
+++ server/api/rooms/board/controller.ts
@@ -1,7 +1,8 @@
+import { roomUsecase } from '$/usecase/roomUsecase';
 import { defineController } from './$relay';
 export default defineController(() => ({
-  get: () => ({ status: 200, body: 'Hello' }),
-  post: ({  }) => {
-    return { status: 200, body:  };
-  },
+  post: async ({ body, user }) => ({
+    status: 201,
+    body: await roomUsecase.clickBoard(body.x, body.y, user.id),
+  }),
 }));
