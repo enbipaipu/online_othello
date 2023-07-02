d2af0023d4b3d14e3b8e25cc887d377b1b3b381d
enbipaipu <s1f10230012@iniad.org>
2023-07-03 07:54:27 +0900

    dannzyonn15

--- server/usecase/userColorUsecase.ts
+++ server/usecase/userColorUsecase.ts
@@ -1,18 +1,35 @@
+// import type { UserId } from '$/commonTypesWithClient/branded';
+// const userColorDict: { black?: UserId; white?: UserId } = {};
+
+// export const userColorUsecase = {
+//   getUserColor: (userId: UserId): number => {
+//     if (userColorDict.black === userId) {
+//       return 1;
+//     } else if (userColorDict.white === userId) {
+//       return 2;
+//     } else if (userColorDict.black === undefined) {
+//       userColorDict.black = userId;
+//       return 1;
+//     } else {
+//       userColorDict.white = userId;
+//       return 2;
+//     }
+//   },
+// };
 import type { UserId } from '$/commonTypesWithClient/branded';
-const userColorDict: { black?: UserId; white?: UserId } = {};
+
+const userColorDict: { [color: string]: UserId } = {};
 
 export const userColorUsecase = {
   getUserColor: (userId: UserId): number => {
-    if (userColorDict.black === userId) {
-      return 1;
-    } else if (userColorDict.white === userId) {
+    const userCount = Object.keys(userColorDict).length;
+
+    if (userCount % 2 === 0) {
+      userColorDict[`color${userCount + 1}`] = userId;
       return 2;
-    } else if (userColorDict.black === undefined) {
-      userColorDict.black = userId;
-      return 1;
     } else {
-      userColorDict.white = userId;
-      return 2;
+      userColorDict[`color${userCount + 1}`] = userId;
+      return 1;
     }
   },
 };
